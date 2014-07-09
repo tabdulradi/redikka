626fef2d150b2428bda68d5ead5f89a46da6f246
Tamer Mohammed Abdul-Radi <tamer@abdulradi.com>
2014-07-09 22:07:44 +0200

    Work on progress. Trying to implement locking per Enitity through cluster.

--- project/common.scala
+++ project/common.scala
@@ -13,32 +13,35 @@ object BuildSettings {
 object Resolvers {
   val typesafe = "typesafe.com" at "http://repo.typesafe.com/typesafe/releases/"
 
+  val typesafeSnapshots = "typesafeSnapshots" at "http://repo.typesafe.com/typesafe/snapshots/"
+
   val sonatype = "sonatype" at "http://oss.sonatype.org/content/repositories/releases"
-  
+
   val chrisdinn = "chrisdinn.github" at "http://chrisdinn.github.io/releases/" // For brando test Client
 
-  val all = Seq(typesafe, sonatype, chrisdinn)
+  val all = Seq(typesafe, typesafeSnapshots, sonatype, chrisdinn)
 }
 
 
 object Dependencies {
 
   val scalatest = "org.scalatest" %% "scalatest" % "2.1.5" % "test"
-  
+
   object Clients {
     val scalaRedis = "net.debasishg" %% "redisclient" % "2.12"
     val brando = "com.digital-achiever" %% "brando" % "1.0.2"
-    
+
     val all = Seq(scalaRedis, brando)
   }
-  
+
   object Akka {
-    val version = "2.3.2"
+    val version = "2.4-20140624-230855"
     val agent = "com.typesafe.akka" %% "akka-agent" % version
-    val testKit = "com.typesafe.akka" %% "akka-testkit" % version
+    val testKit = "com.typesafe.akka" %% "akka-testkit" % version % "test"
     val actor = "com.typesafe.akka" %% "akka-actor" % version
     val cluster = "com.typesafe.akka" %% "akka-cluster" % version
     val contrib = "com.typesafe.akka" %% "akka-contrib" % version
+    val persistance = "com.typesafe.akka" %% "akka-persistence-experimental" % version
   }
 
 }
