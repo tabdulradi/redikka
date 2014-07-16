19fb6e09db595693a4bd518ffe3e1b943d896117
montaro <ahmed.rifaey@gmail.com>
2014-07-16 14:26:40 +0200

    Minor fixes

--- redikka-tcp/src/main/scala/Server.scala
+++ redikka-tcp/src/main/scala/Server.scala
@@ -1,28 +1,28 @@
 package com.abdulradi.redikka.tcp
 
 import java.net.InetSocketAddress
-import akka.actor.{ Actor, ActorRef, Props, ActorLogging }
-import akka.io.{ IO, Tcp }
-import akka.util.ByteString
+import akka.actor.{ActorRef, Actor, ActorLogging}
+import akka.io.{IO, Tcp}
 import com.abdulradi.redikka.core.Redikka
 
 class Server extends Actor with ActorLogging {
 
   import Tcp._
+
   implicit val system = context.system
 
   val port = Server.defaultPort
   log.info("TCP Server has started, trying to listen on port {}", port)
   IO(Tcp) ! Bind(self, new InetSocketAddress("localhost", port))
-  val redikka = Redikka(context.system)
+  val redikka: ActorRef = Redikka(context.system)
 
   def receive = {
     case Bound(localAddress) =>
-      log.info("TCP Server sucessfully bound on address {}", localAddress)
-    case msg @ CommandFailed(_: Bind) =>
+      log.info("TCP Server successfully bound on address {}", localAddress)
+    case msg@CommandFailed(_: Bind) =>
       log.error("TCP Server failed to bind on port {}.", port)
       context stop self
-    case c @ Connected(remote, local) =>
+    case c@Connected(remote, local) =>
       log.debug("TCP Server got a new connection remote={} local={}", remote, local)
       sender() ! Register(context.actorOf(RequestHandler.props(redikka)))
   }
